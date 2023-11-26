69803149f9d7a9100ee51ae29ef2d0399b256fe5
ugurkara1 <91371242+ugurkara1@users.noreply.github.com>
2023-11-26 15:26:00 +0300

    ödev3

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 import cv2
 import numpy as np
-#  kamerayı aç (kamera indeksi 0)
 
 cap=cv2.VideoCapture(0)
+
 while True:
-    _,frame=cap.read()    # Kameradan bir kare oku
+    ret, frame=cap.read()
+    # Gürültüyü azaltmak için blurlama uygula
+    blurred = cv2.GaussianBlur(frame, (5, 5), 0)
 
-    hsv_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)    # BGR karesini HSV renk uzayına dönüştür
+    #gri tonlamaya çevir
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    cv2.imshow("gray:",gray)
+    #kenarları belirleme
+    edges=cv2.Canny(gray,50,150)
+    # Morfolojik operasyonlar uygula
+    kernel = np.ones((5, 5), np.uint8)
+    edges = cv2.dilate(edges, kernel, iterations=1)
+    edges = cv2.erode(edges, kernel, iterations=1)
+    #konturları bulma
+    contours,_=cv2.findContours(edges,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    # Belirli bir alanın altındaki konturları göz ardı et
+    min_contour_area = 500  # Belirlediğiniz bir değer
+    max_contour_area = 1000  # Belirlediğiniz bir değer
+    filtered_contours = [cnt for cnt in contours if min_contour_area < cv2.contourArea(cnt) < max_contour_area]
 
-    # HSV'deki kırmızı rengi belirt
-    low_red=np.array([160,155,84])
-    high_red=np.array([179,255,255])
-    # Belirtilen aralıkta kırmızı rengi içeren bir maske oluştur
-    red_mask=cv2.inRange(hsv_frame,low_red,high_red)
-    # Orijinal kareye kırmızı maskesini uygula, bitwise AND işlemi kullanarak
-    red=cv2.bitwise_and(frame,frame,mask=red_mask)
-    # Orijinal kareyi, kırmızı filtrelenmiş kareyi ve kırmızı maskesini görüntüle
-    cv2.imshow("frame",frame)
-    cv2.imshow("red", red)
-    cv2.imshow("maske", red_mask)
-    # Döngüden çıkmak için 'Esc' tuşu kontrol et
-    k=cv2.waitKey(5) & 0xFF
-    if k==27:
+    #pirinç sayısını ekrana yazdırma
+    print(f"fıstık sayısı:{len(contours)}")
+
+    #konturları orjinal kare üzerine çiz
+    cv2.drawContours(frame,contours,-1,(0,255,0),2)
+    #kareyi göster
+    cv2.imshow("webcam",frame)
+    #çikış için q tuşuna bas
+    if cv2.waitKey(1)& 0xFF ==ord("q"):
         break
-
+cap.release()
 cv2.destroyAllWindows()
